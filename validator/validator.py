e091fd1a78a5d200bfce39c1087eb9e3d73059c0
Jean-Francois Chevrette <jfchevrette@gmail.com>
2021-01-13 14:36:40 -0500

    fix json.load/loads

--- validator/validator.py
+++ validator/validator.py
@@ -341,7 +341,7 @@ def fetch_schema(schema_url):
         r = requests.get(schema_url)
         r.raise_for_status()
         schema = r.text
-        return json.load(schema, encoding='utf-8')
+        return json.loads(schema, encoding='utf-8')
     else:
         raise MissingSchemaFile(schema_url)
 
