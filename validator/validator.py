90b447b90866b2db9d69dba9c38bf6f7d017beab
Di Wang <dwan@redhat.com>
2025-06-06 18:07:59 +1000

    refactor: add typing to validator
    Signed-off-by: Di Wang <dwan@redhat.com>

--- validator/validator.py
+++ validator/validator.py
@@ -2,9 +2,11 @@ import contextlib
 import json
 import logging
 import sys
-from enum import Enum
-from functools import lru_cache
-from typing import IO
+from collections import defaultdict
+from collections.abc import Iterator
+from enum import StrEnum
+from functools import cache
+from typing import IO, Any, NotRequired, TypedDict
 
 import click
 import jsonschema
@@ -33,7 +35,15 @@ class MissingSchemaFileError(Exception):
         super(Exception, self).__init__(message)
 
 
-class ValidatedFileKind(Enum):
+class DuplicateUniqueFieldError(Exception):
+    pass
+
+
+class NotFoundError(Exception):
+    pass
+
+
+class ValidatedFileKind(StrEnum):
     SCHEMA = "SCHEMA"
     DATA_FILE = "FILE"
     REF = "REF"
@@ -41,88 +51,130 @@ class ValidatedFileKind(Enum):
     UNIQUE = "UNIQUE"
 
 
-class ValidationOK:
-    status = True
+class ValidationStatus(StrEnum):
+    OK = "OK"
+    ERROR = "ERROR"
 
-    def __init__(self, kind, filename, schema_url):
+
+class _ValidationResult(TypedDict):
+    summary: str
+    status: ValidationStatus
+    schema_url: NotRequired[str]
+    reason: NotRequired[str]
+    error: NotRequired[str]
+    ref: NotRequired[str]
+    ptr: NotRequired[str]
+    meta_schema_url: NotRequired[str]
+
+
+class ValidationResult(TypedDict):
+    filename: str
+    kind: ValidatedFileKind
+    ref: NotRequired[str]
+    result: _ValidationResult
+
+
+class ValidationOK:
+    def __init__(
+        self,
+        kind: ValidatedFileKind,
+        filename: str,
+        schema_url: str,
+    ) -> None:
         self.kind = kind
         self.filename = filename
         self.schema_url = schema_url
         self.summary = f"OK: {self.filename} ({self.schema_url})"
 
-    def dump(self):
-        return {
-            "filename": self.filename,
-            "kind": self.kind.value,
-            "result": {
-                "summary": self.summary,
-                "status": "OK",
-                "schema_url": self.schema_url,
-            },
-        }
+    def dump(self) -> ValidationResult:
+        return ValidationResult(
+            filename=self.filename,
+            kind=self.kind,
+            result=_ValidationResult(
+                summary=self.summary,
+                status=ValidationStatus.OK,
+                schema_url=self.schema_url,
+            ),
+        )
 
 
 class ValidationRefOK:
-    status = True
-
-    def __init__(self, kind, filename, ref, schema_url):
+    def __init__(
+        self,
+        kind: ValidatedFileKind,
+        filename: str,
+        ref: str,
+        schema_url: str,
+    ) -> None:
         self.kind = kind
         self.filename = filename
         self.schema_url = schema_url
         self.ref = ref
         self.summary = f"OK: {self.filename} ({self.ref}) ({self.schema_url})"
 
-    def dump(self):
-        return {
-            "filename": self.filename,
-            "ref": self.ref,
-            "kind": self.kind.value,
-            "result": {
-                "summary": self.summary,
-                "status": "OK",
-                "schema_url": self.schema_url,
-                "ref": self.ref,
-            },
-        }
+    def dump(self) -> ValidationResult:
+        return ValidationResult(
+            filename=self.filename,
+            kind=self.kind,
+            ref=self.ref,
+            result=_ValidationResult(
+                summary=self.summary,
+                status=ValidationStatus.OK,
+                schema_url=self.schema_url,
+                ref=self.ref,
+            ),
+        )
 
 
 class ValidationError:
-    status = False
-
-    def __init__(self, kind, filename, reason, error, **kwargs):
+    def __init__(
+        self,
+        kind: ValidatedFileKind,
+        filename: str,
+        reason: str,
+        error: Exception,
+        *,
+        meta_schema_url: str | None = None,
+        ref: str | None = None,
+        schema_url: str | None = None,
+        ptr: str | None = None,
+    ) -> None:
         self.kind = kind
         self.filename = filename
         self.reason = reason
         self.error = error
-        self.kwargs = kwargs
+        self.meta_schema_url = meta_schema_url
+        self.ref = ref
+        self.schema_url = schema_url
+        self.ptr = ptr
         self.summary = f"ERROR: {self.filename}"
 
     def dump(self):
-        result = {
-            "summary": self.summary,
-            "status": "ERROR",
-            "reason": self.reason,
-            "error": str(self.error),
-        }
-
-        result.update(self.kwargs)
-
-        return {"filename": self.filename, "kind": self.kind.value, "result": result}
-
-    def error_info(self):
-        if self.error.message:
-            msg = f"{self.reason}\n{self.error.message}"
-        else:
-            msg = self.reason
-
-        return msg
+        return ValidationResult(
+            filename=self.filename,
+            kind=self.kind,
+            result=_ValidationResult(
+                summary=self.summary,
+                status=ValidationStatus.ERROR,
+                reason=self.reason,
+                error=str(self.error),
+                meta_schema_url=self.meta_schema_url,
+                ref=self.ref,
+                schema_url=self.schema_url,
+                ptr=self.ptr,
+            ),
+        )
 
 
 def get_handlers(schemas_bundle):
     return {"": lambda x: schemas_bundle[x]}
 
 
-def validate_schema(schemas_bundle, filename, schema_data):
+def validate_schema(
+    schemas_bundle: dict[str, dict[str, Any]],
+    filename: str,
+    schema_data: dict[str, Any],
+) -> ValidationError | ValidationOK:
     kind = ValidatedFileKind.SCHEMA
 
     logging.info("validating schema: %s", filename)
@@ -157,7 +209,11 @@ def validate_schema(schemas_bundle, filename, schema_data):
     return ValidationOK(kind, filename, meta_schema_url)
 
 
-def validate_file(schemas_bundle, filename, data):
+def validate_file(
+    schemas_bundle: dict[str, dict[str, Any]],
+    filename: str,
+    data: dict[str, Any],
+) -> ValidationError | ValidationOK:
     kind = ValidatedFileKind.DATA_FILE
 
     logging.info("validating file: %s", filename)
@@ -197,55 +253,58 @@ def validate_file(schemas_bundle, filename, data):
     return ValidationOK(kind, filename, schema_url)
 
 
-def validate_unique_fields(bundle: Bundle):  # noqa: C901
-    data_bundle = bundle.data
-    graphql = {}
+def _get_unique_field_names(gql_fields: list[dict[str, Any]]) -> list[str]:
+    return [
+        field["name"]
+        for field in gql_fields
+        if field.get("isUnique")
+    ]
 
-    for item in bundle.list_graphql_types():
-        graphql[item.spec["name"]] = item.spec["fields"]
 
-    datafiles_map = {}
-    for field in graphql["Query"]:
-        if "datafileSchema" in field:
-            datafiles_map[field["type"]] = field["datafileSchema"]
+def validate_unique_fields(
+    bundle: Bundle,
+) -> list[ValidationResult]:
+    graphql = {
+        item.spec["name"]: item.spec["fields"]
+        for item in bundle.list_graphql_types()
+    }
+    datafiles_map = {
+        field["type"]: datafileSchema
+        for field in graphql["Query"]
+        if (datafileSchema := field.get("datafileSchema"))
+    }
 
-    unique_map: dict = {}
-    for gql_type, gql_fields in graphql.items():
-        if gql_type == "Query":
-            continue
+    unique_map = {
+        datafileSchema: _get_unique_field_names(gql_fields)
+        for gql_type, gql_fields in graphql.items()
+        if gql_type != "Query" and (datafileSchema := datafiles_map.get(gql_type))
+    }
 
-        datafile_schema = datafiles_map.get(gql_type)
-        if not datafile_schema:
-            # not top-level schema
-            continue
-
-        unique_map[datafile_schema] = []
-        for field in gql_fields:
-            if field.get("isUnique"):
-                unique_map[datafile_schema].append(field["name"])
-
-    unique_fields: dict = {}
-    for filename, data in data_bundle.items():
+    unique_fields = defaultdict(list)
+    for filename, data in bundle.data.items():
         for field in unique_map.get(data["$schema"], []):
             key = (data["$schema"], field, data.get(field))
-            unique_fields.setdefault(key, [])
             unique_fields[key].append(filename)
 
-    results = []
-    for key, filenames in unique_fields.items():
-        if len(filenames) > 1:
-            error = ValidationError(
-                ValidatedFileKind.UNIQUE,
-                filenames[0],
-                "DUPLICATE_UNIQUE_FIELD",
-                f"The field '{key[1]}' is repeated: {filenames}",
-            )
-            results.append(error.dump())
+    return [
+        ValidationError(
+            ValidatedFileKind.UNIQUE,
+            filenames[0],
+            "DUPLICATE_UNIQUE_FIELD",
+            DuplicateUniqueFieldError(f"The field '{field}' is repeated: {filenames}",),
+            ref=schema,
+            ptr=field,
+        ).dump()
+        for (schema, field, _), filenames in unique_fields.items()
+        if len(filenames) > 1
+    ]
 
-    return results
 
-
-def validate_resource(schemas_bundle, filename, resource):
+def validate_resource(
+    schemas_bundle: dict[str, dict[str, Any]],
+    filename: str,
+    resource: dict[str, Any],
+) -> ValidationError | ValidationOK:
     content = resource["content"]
     if "$schema" not in content:
         return ValidationOK(ValidatedFileKind.NONE, filename, "")
@@ -259,25 +318,32 @@ def validate_resource(schemas_bundle, filename, resource):
     return validate_file(schemas_bundle, filename, data)
 
 
-def validate_ref(schemas_bundle, bundle, filename, data, ptr, ref):
+def validate_ref(
+    schemas_bundle: dict[str, dict[str, Any]],
+    bundle: dict[str, dict[str, Any]],
+    filename: str,
+    data: dict[str, Any],
+    ptr: str,
+    ref: dict[str, Any],
+) -> list[ValidationError | ValidationRefOK | ValidationOK]:
     kind = ValidatedFileKind.REF
 
-    try:
-        ref_data = bundle[ref["$ref"]]
-    except KeyError as e:
-        return ValidationError(kind, filename, "FILE_NOT_FOUND", e, ref=ref["$ref"])
+    if ref["$ref"] not in bundle:
+        return [ValidationError(kind, filename, "FILE_NOT_FOUND", NotFoundError(), ref=ref["$ref"])]
 
-    try:
-        schema = schemas_bundle[data["$schema"]]
-    except KeyError as e:
-        return ValidationError(kind, filename, "SCHEMA_NOT_FOUND", e, ref=ref["$ref"])
+    ref_data = bundle[ref["$ref"]]
+
+    if data["$schema"] not in schemas_bundle:
+        return [ValidationError(kind, filename, "SCHEMA_NOT_FOUND", NotFoundError(), ref=ref["$ref"])]
+
+    schema = schemas_bundle[data["$schema"]]
 
     try:
         schema_infos = get_schema_info_from_pointer(schema, ptr, schemas_bundle)
     except KeyError as e:
-        return ValidationError(
+        return [ValidationError(
             kind, filename, "SCHEMA_DEFINITION_NOT_FOUND", e, ref=ref["$ref"], ptr=ptr
-        )
+        )]
 
     errors = []
     for schema_info in schema_infos:
@@ -296,12 +362,12 @@ def validate_ref(schemas_bundle, bundle, filename, data, ptr, ref):
                         )
                     )
                 else:
-                    return ValidationRefOK(kind, filename, ref["$ref"], data["$schema"])
+                    return [ValidationRefOK(kind, filename, ref["$ref"], data["$schema"])]
             else:
                 try:
                     validator = Draft6Validator(expected_schema)
                     validator.validate(ref_data)
-                    return ValidationRefOK(kind, filename, ref["$ref"], data["$schema"])
+                    return [ValidationRefOK(kind, filename, ref["$ref"], data["$schema"])]
                 except jsonschema.exceptions.ValidationError as e:
                     errors.append(
                         ValidationError(
@@ -312,50 +378,37 @@ def validate_ref(schemas_bundle, bundle, filename, data, ptr, ref):
     return errors
 
 
-@lru_cache
-def fetch_schema(schema_url):
+@cache
+def fetch_schema(schema_url: str) -> dict[str, Any]:
     if schema_url.startswith("http"):
         r = requests.get(schema_url, timeout=10)
         r.raise_for_status()
-        schema = r.text
-        return json.loads(schema)
+        return r.json()
     raise MissingSchemaFileError(schema_url)
 
 
-def find_refs(obj, ptr=None, refs=None):
-    if refs is None:
-        refs = []
-
-    if ptr is None:
-        ptr = ""
-
+def find_refs(
+    obj: Any,
+    ptr: str = "",
+) -> Iterator[tuple[str, dict[str, Any]]]:
     if isinstance(obj, dict):
-        # is this a ref?
         if "$ref" in obj:
-            refs.append((ptr, obj))
+            yield ptr, obj
         else:
             for key, item in obj.items():
                 new_ptr = f"{ptr}/{key}"
-                find_refs(item, new_ptr, refs)
+                yield from find_refs(item, new_ptr)
     elif isinstance(obj, list):
         for index, item in enumerate(obj):
             new_ptr = f"{ptr}/{index}"
-            find_refs(item, new_ptr, refs)
-
-    return refs
+            yield from find_refs(item, new_ptr)
 
 
-def flatten_list(collection):
-    result = []
-    for el in collection:
-        if hasattr(el, "__iter__") and not isinstance(el, str):
-            result.extend(flatten_list(el))
-        else:
-            result.append(el)
-    return result
-
-
-def get_schema_info_from_pointer(schema, ptr, schemas_bundle) -> list[dict]:
+def get_schema_info_from_pointer(
+    schema: dict[str, Any],
+    ptr: str,
+    schemas_bundle: dict[str, dict[str, Any]],
+) -> list[dict[str, Any]]:
     info = schema
 
     ptr_chunks = ptr.split("/")[1:]
@@ -394,7 +447,9 @@ def get_schema_info_from_pointer(schema, ptr, schemas_bundle) -> list[dict]:
     return [info]
 
 
-def validate_bundle(bundle: Bundle) -> list[dict]:
+def validate_bundle(
+    bundle: Bundle,
+) -> list[ValidationResult]:
     # Validate schemas
     results_schemas = [
         validate_schema(bundle.schemas, filename, schema_data).dump()
@@ -418,14 +473,10 @@ def validate_bundle(bundle: Bundle) -> list[dict]:
 
     # validate refs
     results_refs = [
-        r.dump()
-        for r in
-        # validate_ref can return multiple errors, so we flatten the results
-        flatten_list([
-            validate_ref(bundle.schemas, bundle.data, filename, data, ptr, ref)
-            for filename, data in bundle.data.items()
-            for ptr, ref in find_refs(data)
-        ])
+        result.dump()
+        for filename, data in bundle.data.items()
+        for ptr, ref in find_refs(data)
+        for result in validate_ref(bundle.schemas, bundle.data, filename, data, ptr, ref)
     ]
 
     results_graphql_schemas = (
@@ -451,13 +502,17 @@ def validate_bundle(bundle: Bundle) -> list[dict]:
 @click.command()
 @click.option("--only-errors", is_flag=True, help="Print only errors")
 @click.argument("bundlefile", type=click.File("rb"))
-def main(only_errors, bundlefile: IO):
+def main(
+    *,
+    only_errors: bool,
+    bundlefile: IO,
+) -> None:
     bundle = load_bundle(bundlefile)
 
     results = validate_bundle(bundle)
 
     # Calculate errors
-    errors = list(filter(lambda x: x["result"]["status"] == "ERROR", results))
+    errors = [r for r in results if r["result"]["status"] == ValidationStatus.ERROR]
 
     # Output
     if only_errors:
